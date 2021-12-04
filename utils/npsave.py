d0f7e70fc647da2694b80a92b35ade4ca9c0d9f4
jinzx10 <jzx016@hotmail.com>
2021-12-03 18:27:56 -0800

    hw

--- utils/npsave.py
+++ utils/npsave.py
@@ -4,3 +4,7 @@ def ezsave(M, filename):
     f = open(filename, 'w')
     np.savetxt(f, M, fmt='%20.12f')
     f.close()
+
+def savenpy(arr, filename):
+    f = open(filename, 'wb')
+    np.save(f, arr)
